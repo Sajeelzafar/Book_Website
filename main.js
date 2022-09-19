88ed8edd3b557d002942e974ba5e03e26a755b22
Sajeel Zafar <sajeelzafar1995@gmail.com>
2022-09-19 20:22:49 +0500

    Removing books on delete button

--- main.js
+++ main.js
@@ -2,48 +2,56 @@ const addBtn = document.querySelector('.addButton');
 const bookTitle = document.querySelector('.name');
 const bookAuthor = document.querySelector('.author');
 const bookContainer = document.querySelector('.bookContainer');
+const rmvButton = document.querySelector('.removeButton');
 
 let counter = 0;
 let books = [];
 
-function AddBook( bookId, title, author){
-    this.id = bookId;
+function AddBook( title, author){
     this.title = title;
     this.author = author;
-    counter ++;
+    counter++;
+    return;
 }
 
-function displayBook() {
-    const div = document.createElement('div');
-    div.classList.add('bookbox');
-    bookContainer.appendChild(div);
-    const span = document.createElement('span');
-    span.classList.add('bookText');
-    span.innerHTML = bookTitle.value + "<br>" + bookAuthor.value + "<br>";
-    div.appendChild(span);
-    const removeButton = document.createElement('button');
-    removeButton.classList.add('removeButton');
-    removeButton.innerText = "Remove";
-    div.appendChild(removeButton);
-    const divider = document.createElement('hr');
-    div.appendChild(divider);
+function printBook(newBook){
+    for (let i=0; i < newBook.length; i++){
+        const div = document.createElement('div');
+        div.classList.add('bookbox');
+        bookContainer.appendChild(div);
+        const span = document.createElement('span');
+        span.classList.add('bookText');
+        span.innerHTML = bookTitle.value + "<br>" + bookAuthor.value + "<br>";
+        div.appendChild(span);
+        const removeButton = document.createElement('button');
+        removeButton.setAttribute("id", counter);
+        removeButton.innerText = "Remove";
+        div.appendChild(removeButton);
+        const divider = document.createElement('hr');
+        div.appendChild(divider);
+        
+        removeButton.addEventListener('click', (e)=>{
+            bookContainer.removeChild(div);
+            removefunction(e.target.id);
+        }); 
+
+        return
+    }
 }
+
 
 addBtn.addEventListener('click', () =>{
-
-    const newBook = new AddBook(counter ,bookTitle.value, bookAuthor.value);
+    const newBook = new AddBook(bookTitle.value, bookAuthor.value);
     books.push(newBook);
-    displayBook();
-    console.log(books);
-    console.log(counter);
+    printBook(books);
 })
 
-if(counter > 0){
-    const rmvButton = document.querySelector('.removeButton');
-
-    rmvButton.addEventListener('click', (index)=>{
-        console.log("Hello");
-    }); 
+function removefunction(removeIndex){
+    books.splice(removeIndex, 1);
 }
+    
+
+    
+
 
 
