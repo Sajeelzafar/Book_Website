fd5c047e7e50573df35921483c19485e57b28f83
Sajeel Zafar <sajeelzafar1995@gmail.com>
2022-09-19 23:32:41 +0500

    Fix linter Errors

--- main.js
+++ main.js
@@ -2,65 +2,52 @@ const addBtn = document.querySelector('.addButton');
 const bookTitle = document.querySelector('.name');
 const bookAuthor = document.querySelector('.author');
 const bookContainer = document.querySelector('.bookContainer');
-const rmvButton = document.querySelector('.removeButton');
 
 let counter = 0;
-let books = [];
+const books = [];
 
-function AddBook(count, title, author){
-    this.id = count;
-    this.title = title;
-    this.author = author;
-    counter++;
-    console.log("type is ",typeof this.id, "and its value is", this.id, "while counter is", counter);
+function AddBook(count, title, author) {
+  this.id = count;
+  this.title = title;
+  this.author = author;
+  counter += 1;
+}
+
+function removefunction(removeIndex) {
+  books.splice(removeIndex - 1, 1);
+  for (let i = removeIndex - 1; i < books.length; i += 1) {
+    books[i].id -= 1;
+  }
+  counter -= 1;
+}
+
+function printBook(newBook) {
+  for (let i = 0; i < newBook.length; i += 1) {
+    const div = document.createElement('div');
+    div.classList.add('bookbox');
+    bookContainer.appendChild(div);
+    const span = document.createElement('span');
+    span.classList.add('bookText');
+    span.innerHTML = `${bookTitle.value}<br>${bookAuthor.value}<br>`;
+    div.appendChild(span);
+    const removeButton = document.createElement('button');
+    removeButton.setAttribute('id', counter);
+    removeButton.innerText = 'Remove';
+    div.appendChild(removeButton);
+    const divider = document.createElement('hr');
+    div.appendChild(divider);
+
+    removeButton.addEventListener('click', (e) => {
+      bookContainer.removeChild(div);
+      removefunction(e.target.id);
+    });
+
     return;
+  }
 }
 
-function printBook(newBook){
-    for (let i=0; i < newBook.length; i++){
-        const div = document.createElement('div');
-        div.classList.add('bookbox');
-        bookContainer.appendChild(div);
-        const span = document.createElement('span');
-        span.classList.add('bookText');
-        span.innerHTML = bookTitle.value + "<br>" + bookAuthor.value + "<br>";
-        div.appendChild(span);
-        const removeButton = document.createElement('button');
-        removeButton.setAttribute("id", counter);
-        removeButton.innerText = "Remove";
-        div.appendChild(removeButton);
-        const divider = document.createElement('hr');
-        div.appendChild(divider);
-        
-        removeButton.addEventListener('click', (e)=>{
-            bookContainer.removeChild(div);
-            removefunction(e.target.id);
-        }); 
-
-        return
-    }
-}
-
-
-addBtn.addEventListener('click', () =>{
-    const newBook = new AddBook(counter, bookTitle.value, bookAuthor.value);
-    books.push(newBook);
-    console.log(books);
-    printBook(books);
-})
-
-function removefunction(removeIndex){
-    books.splice(removeIndex-1, 1);
-    for(let i = removeIndex-1; i < books.length; i++){
-        books[i].id--;
-    }
-    counter--;
-    console.log("type of ID is: ", typeof removeIndex, removeIndex);
-    console.log(books);
-}
-    
-
-    
-
-
-
+addBtn.addEventListener('click', () => {
+  const newBook = new AddBook(counter, bookTitle.value, bookAuthor.value);
+  books.push(newBook);
+  printBook(books);
+});
