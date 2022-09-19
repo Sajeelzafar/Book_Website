8753418ca1e420f6c882d07b2a0c85e287fa65f8
Sajeel Zafar <sajeelzafar1995@gmail.com>
2022-09-19 18:21:15 +0500

    Displaying the added books on website

--- main.js
+++ main.js
@@ -1,19 +1,36 @@
 const addBtn = document.querySelector('.addButton');
 const bookTitle = document.querySelector('.name');
 const bookAuthor = document.querySelector('.author');
+const bookContainer = document.querySelector('.bookContainer');
 
 let books = [];
-
-
 
 function AddBook( title, author){
     this.title = title;
     this.author = author;
 }
 
+function displayBook() {
+    const div = document.createElement('div');
+    div.classList.add('bookbox');
+    bookContainer.appendChild(div);
+    const span = document.createElement('span');
+    span.classList.add('bookText');
+    span.innerHTML = bookTitle.value + "<br>" + bookAuthor.value + "<br>";
+    div.appendChild(span);
+    const removeButton = document.createElement('button');
+    removeButton.classList.add('removeButton');
+    removeButton.innerText = "Remove";
+    div.appendChild(removeButton);
+    const divider = document.createElement('hr');
+    div.appendChild(divider);
+}
+
 addBtn.addEventListener('click', () =>{
 
     const newBook = new AddBook(bookTitle.value, bookAuthor.value);
     books.push(newBook);
+    displayBook();
     console.log(books);
 })
+
