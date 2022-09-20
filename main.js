1344635a4a398ccd44648e90f582edaa66a9d55f
Sajeel Zafar <sajeelzafar1995@gmail.com>
2022-09-20 12:53:42 +0500

    Merging and Deleting unnecessary files

--- main.js
+++ main.js
@@ -3,51 +3,112 @@ const bookTitle = document.querySelector('.name');
 const bookAuthor = document.querySelector('.author');
 const bookContainer = document.querySelector('.bookContainer');
 
-let counter = 0;
-const books = [];
+// DISPLAY SAVED DATA ON LOAD
+document.addEventListener('DOMContentLoaded', () => {
+  let arrBooks;
 
-function AddBook(count, title, author) {
-  this.id = count;
-  this.title = title;
-  this.author = author;
-  counter += 1;
-}
-
-function removefunction(removeIndex) {
-  books.splice(removeIndex - 1, 1);
-  for (let i = removeIndex - 1; i < books.length; i += 1) {
-    books[i].id -= 1;
+  if (localStorage.getItem('books') === 'null') {
+    arrBooks = [];
+  } else {
+    arrBooks = JSON.parse(localStorage.getItem('books'));
   }
-  counter -= 1;
-}
 
-function printBook(newBook) {
-  for (let i = 0; i < newBook.length; i += 1) {
+  arrBooks.forEach((book) => {
     const div = document.createElement('div');
-    div.classList.add('bookbox');
+    div.classList.add('bookBox');
     bookContainer.appendChild(div);
-    const span = document.createElement('span');
-    span.classList.add('bookText');
-    span.innerHTML = `${bookTitle.value}<br>${bookAuthor.value}<br>`;
-    div.appendChild(span);
+    const textContainer = document.createElement('div');
+    textContainer.classList.add('bookText');
+    textContainer.innerHTML = `
+
+              <p>${book.title}</p>
+              <p>${book.author} </p>`;
+
+    div.appendChild(textContainer);
     const removeButton = document.createElement('button');
-    removeButton.setAttribute('id', counter);
     removeButton.innerText = 'Remove';
     div.appendChild(removeButton);
-    const divider = document.createElement('hr');
-    div.appendChild(divider);
+  });
+});
 
-    removeButton.addEventListener('click', (e) => {
-      bookContainer.removeChild(div);
-      removefunction(e.target.id);
-    });
-
-    return;
-  }
+// BOOK PROTOTYPE
+function AddBook(title, author) {
+  this.title = title;
+  this.author = author;
 }
 
+// PRINT BOOK ON TOP
+function printBook() {
+  const div = document.createElement('div');
+  div.classList.add('bookBox');
+  bookContainer.appendChild(div);
+  const textContainer = document.createElement('div');
+  textContainer.classList.add('bookText');
+  textContainer.innerHTML = `
+
+            <p>${bookTitle.value}</p>
+            <p>${bookAuthor.value} </p>`;
+
+  div.appendChild(textContainer);
+  const removeButton = document.createElement('button');
+  removeButton.innerText = 'Remove';
+  div.appendChild(removeButton);
+  const divider = document.createElement('hr');
+  div.appendChild(divider);
+}
+
+// SAVE TO LOCAL STORAGE
+function saveToLocal(book) {
+  let arrBooks;
+
+  if (localStorage.getItem('books') === null) {
+    arrBooks = [];
+  } else {
+    arrBooks = JSON.parse(localStorage.getItem('books'));
+  }
+  arrBooks.push(book);
+  localStorage.setItem('books', JSON.stringify(arrBooks));
+}
+
+// DELETE FROM LOCALSTORAGE
+function deleteInStorage(book) {
+  let arrBooks;
+
+  if (localStorage.getItem('books') === null) {
+    arrBooks = [];
+  } else {
+    arrBooks = JSON.parse(localStorage.getItem('books'));
+  }
+  const elementText = book.children[0].children[0].innerText;
+  const spliceBook = arrBooks.map((element) => element.title).indexOf(elementText);
+  arrBooks.splice(spliceBook, 1);
+  localStorage.setItem('books', JSON.stringify(arrBooks));
+}
+
+// ADD ELEMENT TO LIST
 addBtn.addEventListener('click', () => {
-  const newBook = new AddBook(counter, bookTitle.value, bookAuthor.value);
-  books.push(newBook);
-  printBook(books);
+  if (bookTitle.value !== '' && bookAuthor !== '') {
+    const newBook = new AddBook(bookTitle.value, bookAuthor.value);
+    saveToLocal(newBook);
+    printBook();
+  } else {
+    document.querySelector('.error').innerText = 'Please Enter a the Title and the Author';
+    document.querySelector('.error').style.display = 'flex';
+  }
+});
+
+// DELETE ELEMENT FROM DOM
+function deleteFromDom(e) {
+  const item = e.target;
+  const parent = item.parentElement;
+
+  parent.remove();
+  deleteInStorage(parent);
+}
+
+bookContainer.addEventListener('click', deleteFromDom);
+document.querySelectorAll('input').forEach((input) => {
+  input.addEventListener('focus', () => {
+    document.querySelector('.error').style.display = 'none';
+  });
 });
